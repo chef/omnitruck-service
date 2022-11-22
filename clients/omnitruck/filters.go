318e3d127c6046b9c5fbe3a49b8430dd17f3d145
Will Fisher <will.fisher@progress.com>
2022-11-22 11:50:57 -0900

    Ensure that the OS endpoint only returns the opensource product names
    Signed-off-by: Will Fisher <will.fisher@progress.com>

--- clients/omnitruck/filters.go
+++ clients/omnitruck/filters.go
@@ -13,6 +13,19 @@ func FilterList[T comparable](s []T, filter func(T) bool) []T {
 	return out[:counter]
 }
 
+func SelectList[T comparable](s []T, filter func(T) bool) []T {
+	out := make([]T, len(s))
+
+	counter := 0
+	for i := 0; i < len(s); i++ {
+		if filter(s[i]) {
+			out[counter] = s[i]
+			counter++
+		}
+	}
+	return out[:counter]
+}
+
 func FilterProductList[T comparable](s []T, product string, filter func(string, T) bool) []T {
 	out := make([]T, len(s))
 
