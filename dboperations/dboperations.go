7da6875554aa406cde4b0511201f950b44741227
vviveksharma <visharma@progress.com>
2023-08-14 12:21:21 +0530

    Added the pointers to function response and changed metadata api
    Signed-off-by: vviveksharma <visharma@progress.com>

--- dboperations/dboperations.go
+++ dboperations/dboperations.go
@@ -9,7 +9,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/aws/aws-sdk-go/service/dynamodb/expression"
-	"github.com/chef/omnitruck-service/middleware/db"
+	dbconnection "github.com/chef/omnitruck-service/middleware/db"
 	"github.com/chef/omnitruck-service/models"
 )
 
