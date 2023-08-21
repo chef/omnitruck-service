d81d768c0c9327bbde077b4132a18878594dc790
vviveksharma <visharma@progress.com>
2023-08-21 13:38:48 +0530

    The fix for opensource to hit .metdata.json
    Signed-off-by: vviveksharma <visharma@progress.com>

--- services/helpers.go
+++ services/helpers.go
@@ -2,9 +2,12 @@ package services
 
 import (
 	"net/url"
+	"strings"
 
 	"github.com/chef/omnitruck-service/clients/omnitruck"
 )
+
+const substring = ".metadata.json"
 
 func buildEndpointUrl(baseUrl string, endpoint string, params *omnitruck.RequestParams) *url.URL {
 	u, _ := url.Parse(baseUrl)
@@ -30,4 +33,24 @@ func getRequestParams(c omnitruck.FiberContext) *omnitruck.RequestParams {
 		LicenseId:       c.Query("license_id"),
 		Eol:             c.Query("eol", "false"),
 	}
+}
+
+func verifyRequestType(params *omnitruck.RequestParams) bool {
+	if strings.Contains(params.Architecture, substring) {
+		params.Architecture = strings.Replace(params.Architecture, substring, "", 1)
+		return true
+	} else if strings.Contains(params.Platform, substring) {
+		params.Platform = strings.Replace(params.Platform, substring, "", 1)
+		return true
+	} else if strings.Contains(params.PlatformVersion, substring) {
+		params.PlatformVersion = strings.Replace(params.PlatformVersion, substring, "", 1)
+		return true
+	} else if strings.Contains(params.Eol, substring) {
+		params.Eol = strings.Replace(params.Eol, substring, "", 1)
+		return true
+	} else if strings.Contains(params.Version, substring) {
+		params.Version = strings.Replace(params.Version, substring, "", 1)
+		return true
+	}
+	return false
 }
