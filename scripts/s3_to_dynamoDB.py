a4f83d4519c6abeee35a721c18f7d50260a54597
vviveksharma <visharma@progress.com>
2025-06-02 18:17:40 +0530

    added multi product support
    Signed-off-by: vviveksharma <visharma@progress.com>

--- scripts/s3_to_dynamoDB.py
+++ scripts/s3_to_dynamoDB.py
@@ -9,7 +9,7 @@ def convert_to_dynamodb_format(data):
         return {k: {'M': convert_to_dynamodb_format(v)} if isinstance(v, dict) else {'S': str(v)} for k, v in data.items()}
     return {'S': str(data)}
 
-def lambda_handler(event,context):
+def lambda_handler(event, context):
     s3_client = boto3.client('s3')
     dynamodb_client = boto3.client('dynamodb')
     
@@ -23,87 +23,77 @@ def lambda_handler(event,context):
             object_key = obj['Key']
             
             if object_key.endswith('metadata.json'):
-                print(f"\nProcessing S3 object: {object_key}")
                 parts = object_key.split('/')
                 if len(parts) < 3:
                     print(f"Skipping object with unexpected key structure: {object_key}")
                     continue
                 
-                channel = parts[0]
-                product_from_key = parts[1]
-                version_from_key = parts[2]
                 response = s3_client.get_object(Bucket=bucket_name, Key=object_key)
                 file_content = response['Body'].read().decode('utf-8')
+                
+                # Convert file_content to JSON
                 json_content = json.loads(file_content)
+                
+                # Navigate to the stable folder dynamically
                 stable_data = json_content.get('stable', {})
-                product_level_data = stable_data.get(product_from_key, {})
-                version_level_data = product_level_data.get(version_from_key, {})
-
-                print(f"Product: {product_from_key}, Version: {version_from_key}")
-                
-                if not version_level_data:
-                    print(f"Could not find data for product '{product_from_key}' and version '{version_from_key}' in {object_key}")
-                    continue
-                linux_contents = version_level_data.get('linux', {})
-                windows_contents = version_level_data.get('windows', {})
-
-                linux_dynamodb_format = convert_to_dynamodb_format(linux_contents)
-                windows_dynamodb_format = convert_to_dynamodb_format(windows_contents)
-
-                dynamodb_client.put_item(
-                    TableName=METADATA_TABLE,
-                    Item={
-                        'product': {'S': product_from_key},
-                        'version': {'S': version_from_key},
-                        'metadata': {'M': {
-                            'linux': {'M': linux_dynamodb_format},
-                            'windows': {'M': windows_dynamodb_format}
-                        }}
-                    }
-                )
-                
-                if linux_contents:
-                    for arch, arch_data in linux_contents.items():
-                        if isinstance(arch_data, dict):
-                            for package_type, package_details in arch_data.items():
-                                print(f"    Package Type: {package_type}")
-                        else:
-                            print(f"    Unexpected data format for architecture {arch}: {arch_data}")
+                for product_name, product_versions in stable_data.items():
+                    
+                    for version, version_data in product_versions.items():
                         
+                        linux_contents = version_data.get('linux', {})
+                        windows_contents = version_data.get('windows', {})
+
+                        linux_dynamodb_format = convert_to_dynamodb_format(linux_contents)
+                        windows_dynamodb_format = convert_to_dynamodb_format(windows_contents)
+
+
                         dynamodb_client.put_item(
-                            TableName=PACKAGE_MANAGER_TABLE,
+                            TableName=METADATA_TABLE,
                             Item={
-                                'product': {'S': product_from_key},
-                                'os_type': {'S': "linux"},
-                                'package': {'S': package_type},
+                                'product': {'S': product_name},
+                                'version': {'S': version},
+                                'metadata': {'M': {
+                                    'linux': {'M': linux_dynamodb_format},
+                                    'windows': {'M': windows_dynamodb_format}
+                                }}
                             }
                         )
-                else:
-                    print("  No Linux content found.")
-
-                if windows_contents:
-                    for arch, arch_data in windows_contents.items():
-                        if isinstance(arch_data, dict):
-                            for package_type, package_details in arch_data.items():
-                                print(f"    Package Type: {package_type}")
+                        
+                        if linux_contents:
+                            for arch, arch_data in linux_contents.items():
+                                if isinstance(arch_data, dict):
+                                    for package_type, package_details in arch_data.items():
+                                        dynamodb_client.put_item(
+                                            TableName=PACKAGE_MANAGER_TABLE,
+                                            Item={
+                                                'packages': {'S': package_type},
+                                            }
+                                        )
+                                else:
+                                    print(f"Unexpected data format for Linux architecture {arch}: {arch_data}")
                         else:
-                            print(f"    Unexpected data format for architecture {arch}: {arch_data}")
-                    dynamodb_client.put_item(
-                        TableName=PACKAGE_MANAGER_TABLE,
-                        Item={
-                            'product': {'S': product_from_key},
-                            'os_type': {'S': "windows"},
-                            'package': {'S': package_type},
-                        }
-                    )    
-                else:
-                    print("  No Windows content found.")
+                            print("No Linux content found.")
 
-        print("\nAll relevant metadata.json files processed successfully.")
+                        if windows_contents:
+                            for arch, arch_data in windows_contents.items():
+                                if isinstance(arch_data, dict):
+                                    for package_type, package_details in arch_data.items():
+                                        dynamodb_client.put_item(
+                                            TableName=PACKAGE_MANAGER_TABLE,
+                                            Item={
+                                                'packages': {'S': package_type},
+                                            }
+                                        )
+                                else:
+                                    print(f"Unexpected data format for Windows architecture {arch}: {arch_data}")
+                        else:
+                            print("No Windows content found.")
+
+        print("All relevant metadata.json files processed successfully.")
         
         return {
             'statusCode': 200,
-            'body': json.dumps('Successfully processed metadata.json files.')
+            'body': json.dumps('Successfully processed metadata.json file.')
         }
     
     except Exception as e:
