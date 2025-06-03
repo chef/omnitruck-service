e510bdea76b1ac8d0fa8bc3404622af6b10978ec
vviveksharma <visharma@progress.com>
2025-06-03 14:42:11 +0530

    Added logging
    Signed-off-by: vviveksharma <visharma@progress.com>

--- scripts/s3_to_dynamoDB.py
+++ scripts/s3_to_dynamoDB.py
@@ -1,5 +1,11 @@
 import boto3
 import json
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
 
 METADATA_TABLE = 'metadata'
 PACKAGE_MANAGER_TABLE = 'package-manager-dev'
@@ -15,6 +21,8 @@ def lambda_handler(event, context):
     
     try:
         bucket_name = event['bucket_name']
+        logging.info(f"Bucket Name: {bucket_name}")
+        
         objects = s3_client.list_objects_v2(Bucket=bucket_name)
         if 'Contents' not in objects:
             raise ValueError(f"No objects found in the bucket: {bucket_name}")
@@ -23,18 +31,17 @@ def lambda_handler(event, context):
             object_key = obj['Key']
             
             if object_key.endswith('metadata.json'):
+                logging.info(f"Processing S3 object: {object_key}")
                 parts = object_key.split('/')
                 if len(parts) < 3:
-                    print(f"Skipping object with unexpected key structure: {object_key}")
+                    logging.warning(f"Skipping object with unexpected key structure: {object_key}")
                     continue
                 
                 response = s3_client.get_object(Bucket=bucket_name, Key=object_key)
                 file_content = response['Body'].read().decode('utf-8')
                 
-                # Convert file_content to JSON
                 json_content = json.loads(file_content)
                 
-                # Navigate to the stable folder dynamically
                 stable_data = json_content.get('stable', {})
                 for product_name, product_versions in stable_data.items():
                     
@@ -46,7 +53,7 @@ def lambda_handler(event, context):
                         linux_dynamodb_format = convert_to_dynamodb_format(linux_contents)
                         windows_dynamodb_format = convert_to_dynamodb_format(windows_contents)
 
-
+                        logging.info(f"Adding metadata for product: {product_name}, version: {version}")
                         dynamodb_client.put_item(
                             TableName=METADATA_TABLE,
                             Item={
@@ -63,6 +70,7 @@ def lambda_handler(event, context):
                             for arch, arch_data in linux_contents.items():
                                 if isinstance(arch_data, dict):
                                     for package_type, package_details in arch_data.items():
+                                        logging.info(f"Adding linux package: {package_type}")
                                         dynamodb_client.put_item(
                                             TableName=PACKAGE_MANAGER_TABLE,
                                             Item={
@@ -70,14 +78,15 @@ def lambda_handler(event, context):
                                             }
                                         )
                                 else:
-                                    print(f"Unexpected data format for Linux architecture {arch}: {arch_data}")
+                                    logging.warning(f"Unexpected data format for Linux architecture {arch}: {arch_data}")
                         else:
-                            print("No Linux content found.")
+                            logging.info("No Linux content found.")
 
                         if windows_contents:
                             for arch, arch_data in windows_contents.items():
                                 if isinstance(arch_data, dict):
                                     for package_type, package_details in arch_data.items():
+                                        logging.info(f"Adding windows package: {package_type}")
                                         dynamodb_client.put_item(
                                             TableName=PACKAGE_MANAGER_TABLE,
                                             Item={
@@ -85,11 +94,11 @@ def lambda_handler(event, context):
                                             }
                                         )
                                 else:
-                                    print(f"Unexpected data format for Windows architecture {arch}: {arch_data}")
+                                    logging.warning(f"Unexpected data format for Windows architecture {arch}: {arch_data}")
                         else:
-                            print("No Windows content found.")
+                            logging.info("No Windows content found.")
 
-        print("All relevant metadata.json files processed successfully.")
+        logging.info("All relevant metadata.json files processed successfully.")
         
         return {
             'statusCode': 200,
@@ -97,7 +106,7 @@ def lambda_handler(event, context):
         }
     
     except Exception as e:
-        print(f"Error: {str(e)}")
+        logging.error(f"Error while running the lambda script: {str(e)}")
         return {
             'statusCode': 500,
             'body': json.dumps(f"Error: {str(e)}")
