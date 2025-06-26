a338020c47ab19e4bc5ed1c2238b8fe538e2cfa7
vviveksharma <visharma@progress.com>
2025-06-26 15:42:58 +0530

    Improved logging:
    Signed-off-by: vviveksharma <visharma@progress.com>

--- scripts/s3_to_dynamoDB.py
+++ scripts/s3_to_dynamoDB.py
@@ -14,7 +14,7 @@ ASSUME_ROLE_ARN = "arn:aws:iam::712624343120:role/cross-account-s3-role"
 sts_client = boto3.client('sts')
 
 def assume_role():
-    logging.info("Assuming role for cross-account access")
+    logger.info("Assuming role for cross-account access")
     return sts_client.assume_role(
         RoleArn=ASSUME_ROLE_ARN,
         RoleSessionName="CrossAccountAccessSession"
@@ -32,12 +32,12 @@ def create_dynamodb_client():
     return boto3.client('dynamodb')
 
 def extract_channel(object_key):
-    logging.info(f"Extracting channel from object key: {object_key}")
+    logger.info(f"Extracting channel from object key: {object_key}")
     match = re.search(r'\b(stable|current)\b', object_key)
     return match.group(1) if match else None
 
 def convert_to_dynamodb_format(data):
-    logging.info("Converting data to DynamoDB format")
+    logger.info("Converting data to DynamoDB format")
     if isinstance(data, dict):
         return {k: {"M": convert_to_dynamodb_format(v)} if isinstance(v, dict) else {"S": str(v)} for k, v in data.items()}
     return {'S': str(data)}
@@ -49,14 +49,14 @@ def process_metadata_file(s3_client, dynamodb_client, bucket_name, object_key, c
     try:
         json_content = json.loads(file_content)
     except json.JSONDecodeError as e:
-        logging.error("Failed to parse JSON file: %s", str(e))
+        logger.error("Failed to parse JSON file: %s", str(e))
         raise
 
     for product_name, product_versions in json_content.items():
-        logging.info(f"Processing product: {product_versions}")
+        logger.debug(f"Processing product '{product_name}' with versions: {product_versions}")
         for version, version_data in product_versions.items():
             filtered_metadata = {key: value for key, value in version_data.items() if key != 'product-version-metadata'}
-            logging.info(f"Filtered metadata for {product_name} version {version}: {filtered_metadata}")
+            logger.debug(f"Filtered metadata for {product_name} version {version}: {filtered_metadata}")
 
             for table_suffix in ['acceptance', 'production']:
                 table_name = f"package-details-{channel}-{table_suffix}"
@@ -68,12 +68,13 @@ def process_metadata_file(s3_client, dynamodb_client, bucket_name, object_key, c
                         'metadata': {'M': convert_to_dynamodb_format(filtered_metadata)}
                     }
                 )
-            logging.info("Data inserted into metadata tables successfully.")
-            logging.info(f"Processing package manager data for {product_name} version {version}")
+            logger.info("Successfully updated DynamoDB tables for product: %s, version: %s", product_name, version)
             process_package_manager_data(dynamodb_client, version_data)
+            logger.info("Processed package manager data successfully")
 
 def process_package_manager_data(dynamodb_client, version_data):
-    logging.info("Processing package manager data")
+    """Process package manager data and update DynamoDB."""
+    logger.info("Processing package manager data")
     for platform, platform_data in version_data.items():
         if isinstance(platform_data, dict):
             for arch, arch_data in platform_data.items():
@@ -83,57 +84,55 @@ def process_package_manager_data(dynamodb_client, version_data):
                             if package_type in ARCH_LIST:
                                 continue
 
-                            for table_name in PACKAGE_MANAGER_TABLES:
-                                dynamodb_client.put_item(
-                                    TableName=table_name,
-                                    Item={
-                                        'packages': {'S': package_type}
-                                    }
-                                )
-                            logging.info("Package manager data processed successfully.")
+                        for table_name in PACKAGE_MANAGER_TABLES:
+                            dynamodb_client.put_item(
+                                TableName=table_name,
+                                Item={
+                                    'packages': {'S': package_type}
+                                }
+                            )
                 else:
-                    logging.debug(f"Unexpected data format for architecture {arch}: {arch_data}")
+                    logger.debug(f"Unexpected data format for architecture {arch}: {arch_data}")
         else:
-            logging.debug(f"Unexpected data format for platform {platform}: {platform_data}")
+            logger.debug(f"Unexpected data format for platform {platform}: {platform_data}")
 
 def lambda_handler(event, context):
     try:
 
         assumed_role = assume_role()
-        logging.info("Assumed role successfully.")
+        logger.info("Assumed role successfully.")
 
         s3_client = create_s3_client(assumed_role['Credentials'])
         dynamodb_client = create_dynamodb_client()
 
-        logging.info("Lambda function started processing S3 bucket objects.")
+        logger.info("Lambda function started processing S3 bucket objects.")
 
         try:
-            logging.info("Received event: %s", json.dumps(event, indent=2))
+            logger.info("Received event: %s", json.dumps(event, indent=2))
         except Exception as e:
-            logging.error("Failed to format event as JSON: %s", str(e))
+            logger.error("Failed to format event as JSON: %s", str(e))
 
         bucket_name = event['Records'][0]['s3']['bucket']['name']
         object_key = event['Records'][0]['s3']['object']['key']
 
-        logging.info(f"Bucket name: {bucket_name}")
-        logging.info(f"Object key: {object_key}")
+        logger.debug(f"Bucket name: {bucket_name}")
+        logger.debug(f"Object key: {object_key}")
 
         channel = extract_channel(object_key)
         if not channel:
-            logging.info(f"the channel from which we got:{object_key}")
-            logging.debug("Channel not found in object key.")
+            logger.debug(f"Channel not found in object key: {object_key}")
             return
 
         if object_key.endswith('metadata.json'):
             pversion_folder = os.path.dirname(object_key) + '/'
         else:
-            logging.info(f"metadata.json not created or updated: {object_key}")
+            logger.info(f"metadata.json not created or updated: {object_key}")
             return
 
         objects = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=pversion_folder)
 
         if 'Contents' not in objects:
-            logging.info(f"No objects found in the bucket: {bucket_name} under channel: {channel}")
+            logger.debug(f"No objects found in the bucket: {bucket_name} under channel: {channel}")
             return
 
         for obj in objects['Contents']:
@@ -144,9 +143,7 @@ def lambda_handler(event, context):
                 if len(parts) < 3 or object_channel != channel:
                     continue
 
-                logging.debug(f"Processing object: {object_key}")
                 process_metadata_file(s3_client, dynamodb_client, bucket_name, object_key, channel)
-                logging.info("Processed metadata file successfully.")
 
         return {
             'statusCode': 200,
@@ -154,7 +151,7 @@ def lambda_handler(event, context):
         }
 
     except Exception as e:
-        logging.error(f"Error: {str(e)}")
+        logger.error(f"Error: {str(e)}")
         return {
             'statusCode': 500,
             'body': json.dumps(f"Error: {str(e)}")
