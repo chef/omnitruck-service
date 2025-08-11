5841a85668393d40860f459f28543414a921347c
Shanmugapriya <shramana@progress.com>
2025-08-11 12:45:20 +0530

    CHEF-24963 : Updated AWS SDK
    Signed-off-by: Shanmugapriya <shramana@progress.com>

--- utils/awsutils/aws.go
+++ utils/awsutils/aws.go
@@ -1,95 +1,61 @@
 package awsutils
 
 import (
+	"context"
 	"encoding/base64"
 	"log"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awserr"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/secretsmanager"
-	"github.com/chef/omnitruck-service/config"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
+	omnitruckConfig "github.com/chef/omnitruck-service/config"
 )
 
 type AwsUtilsImpl struct{}
-
 type AwsUtils interface {
-	GetNewSession(config config.AWSConfig) (*session.Session, error)
+	GetNewConfig(ctx context.Context, awsConfig omnitruckConfig.AWSConfig) (aws.Config, error)
+}
+
+// GetNewConfig returns an aws.Config for aws-sdk-go-v2
+func (au *AwsUtilsImpl) GetNewConfig(ctx context.Context, awsConfig omnitruckConfig.AWSConfig) (aws.Config, error) {
+	return config.LoadDefaultConfig(ctx,
+		config.WithRegion(awsConfig.Region),
+		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(awsConfig.AccessKey, awsConfig.SecretKey, "")),
+	)
 }
 
 func NewAwsUtils() *AwsUtilsImpl {
 	return &AwsUtilsImpl{}
 }
 
-func (au *AwsUtilsImpl) GetNewSession(config config.AWSConfig) (*session.Session, error) {
-	session, err := session.NewSessionWithOptions(session.Options{
-		Config: aws.Config{
-			Credentials: credentials.NewStaticCredentials(config.AccessKey, config.SecretKey, ""),
-			Region:      aws.String(config.Region),
-		},
-	})
-	if err != nil {
-		log.Printf("Error while creating session: %v", err)
-		return nil, err
-	}
-	return session, nil
-}
-
 var GetSecret = func(secretKey, region string) (secret string) {
-	sess, err := session.NewSession()
+	ctx := context.TODO()
+	cfg, err := config.LoadDefaultConfig(ctx, config.WithRegion(region))
 	if err != nil {
 		// Handle session creation error
 		log.Println(err.Error())
 		return
 	}
-	svc := secretsmanager.New(sess,
-		aws.NewConfig().WithRegion(region))
+	svc := secretsmanager.NewFromConfig(cfg)
 	input := &secretsmanager.GetSecretValueInput{
-		SecretId: aws.String(secretKey),
+		SecretId: &secretKey,
 	}
-
-	result, err := svc.GetSecretValue(input)
-
+	result, err := svc.GetSecretValue(ctx, input)
 	if err != nil {
-		if aerr, ok := err.(awserr.Error); ok {
-			switch aerr.Code() {
-			case secretsmanager.ErrCodeDecryptionFailure:
-				// Secrets Manager can't decrypt the protected secret text using the provided KMS key.
-				log.Println(secretsmanager.ErrCodeDecryptionFailure, aerr.Error())
-
-			case secretsmanager.ErrCodeInternalServiceError:
-				// An error occurred on the server side.
-				log.Println(secretsmanager.ErrCodeInternalServiceError, aerr.Error())
-
-			case secretsmanager.ErrCodeInvalidParameterException:
-				// You provided an invalid value for a parameter.
-				log.Println(secretsmanager.ErrCodeInvalidParameterException, aerr.Error())
-
-			case secretsmanager.ErrCodeInvalidRequestException:
-				// You provided a parameter value that is not valid for the current state of the resource.
-				log.Println(secretsmanager.ErrCodeInvalidRequestException, aerr.Error())
-
-			case secretsmanager.ErrCodeResourceNotFoundException:
-				// We can't find the resource that you asked for.
-				log.Println(secretsmanager.ErrCodeResourceNotFoundException, aerr.Error())
-			}
-		} else {
-			log.Println(err.Error())
-		}
+		log.Println("Error getting secret value:", err)
 		return
 	}
-
-	var secretString string
 	if result.SecretString != nil {
-		secretString = *result.SecretString
-	} else {
+		secret = *result.SecretString
+	} else if result.SecretBinary != nil {
 		decodedBinarySecretBytes := make([]byte, base64.StdEncoding.DecodedLen(len(result.SecretBinary)))
 		_, err := base64.StdEncoding.Decode(decodedBinarySecretBytes, result.SecretBinary)
 		if err != nil {
 			log.Println("Base64 Decode Error:", err)
 			return
 		}
+		secret = string(decodedBinarySecretBytes)
 	}
-	return secretString
+	return
 }
